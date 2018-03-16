cf00295e367af1b0b00ed545449af868cbe38b7b
Victor Andrei <v_andu@yahoo.com>
2018-03-16 16:35:34 +0200

    feedback v2

--- src/main/java/com/example/config/ProductionConfig.java
+++ src/main/java/com/example/config/ProductionConfig.java
@@ -1,6 +1,9 @@
 package com.example.config;
 
 
+import com.example.backend.service.EmailService;
+import com.example.backend.service.SmtpEmailService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
@@ -10,5 +13,9 @@ import org.springframework.context.annotation.PropertySource;
 @PropertySource("file:///${user.home}/Desktop/Bootstrap app/webApp/application-prod.properties")
 public class ProductionConfig {
 
+    @Bean
+    public EmailService emailService(){
+        return new SmtpEmailService();
+    }
 
 }
