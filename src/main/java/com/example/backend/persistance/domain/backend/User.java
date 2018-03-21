20d9815482d30d3471ffd431c9044a79da7e9f42
vasiliu.victor <v_andu@yahoo.com>
2018-03-21 15:45:17 +0200

    unique index

--- src/main/java/com/example/backend/persistance/domain/backend/User.java
+++ src/main/java/com/example/backend/persistance/domain/backend/User.java
@@ -26,9 +26,9 @@ public class User implements Serializable,UserDetails {
     @Column(unique = true)
     private String username;
 
-    @Column(unique = true)
     private String password;
 
+    @Column(unique = true)
     private String email;
 
     @Column(name = "first_name")
