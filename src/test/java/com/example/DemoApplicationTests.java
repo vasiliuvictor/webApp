8bf95147cdb5aa5f988619084920ea515f73a3b4
vasiliu.victor <v_andu@yahoo.com>
2018-03-21 11:48:44 +0200

    Testing succesfully

--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -1,29 +1,28 @@
 package com.example;
 
-import com.example.webapp.i18n.I18NService;
-import org.h2.command.dml.Replace;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import com.example.webapp.i18n.I18NService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = DemoApplication.class)
+@SpringApplicationConfiguration(classes = DemoApplication.class)
+@ActiveProfiles("prod")
 public class DemoApplicationTests {
 
     @Autowired
     private I18NService i18NService;
 
     @Test
-    public void testMessageByLocaleService() throws Exception{
-        String expectedResult = "Welcome to the Mars";
-        String messageId ="index.main.callout";
+    public void testMessageByLocaleService() throws Exception {
+        String expectedResult = "Bootstrap starter template";
+        String messageId = "index.main.callout";
         String actual = i18NService.getMssage(messageId);
-        Assert.assertEquals("the actual and expected Strings don't match",expectedResult,actual);
+        Assert.assertEquals("The actual and expected Strings don't match", expectedResult, actual);
     }
 
 }
