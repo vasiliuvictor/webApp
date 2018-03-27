28fbcdb3a99a577e30f9a265d383ea1e9257c4fd
vasiliu.victor <v_andu@yahoo.com>
2018-03-27 12:53:28 +0300

    Externalize forgotPassword properties

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -10,6 +10,7 @@ import com.example.utils.UserUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -26,18 +27,37 @@ public class DemoApplication implements CommandLineRunner{
 	@Autowired
 	private UserService userService;
 
+	@Value("${webmaster.username}")
+	private String webmasterUsername;
+
+	@Value("${webmaster.password}")
+	private String webmasterPassword;
+
+	@Value("${webmaster.email}")
+	private String webmasterEmail;
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+
 	@Override
 	public void run(String... strings) throws Exception {
 
+		User ForgotUser =UserUtils.createBasicUser(webmasterUsername,webmasterEmail);
+		ForgotUser.setPassword(webmasterPassword);
+		Set<UserRole> userForgotRoles = new HashSet<>();
+		userForgotRoles.add(new UserRole(ForgotUser, new Role(RolesEnum.ADMIN)));
+		LOG.debug("Creating a new forgotUser {}",ForgotUser.getUsername());
+		userService.createUser(ForgotUser,PlansEnum.PRO,userForgotRoles);
+		LOG.info("user {} created", ForgotUser.getUsername());
+
 		String username ="Victor";
 		String email = "newEmail@yahoo.com";
+
 		User user = UserUtils.createBasicUser(username,email);
 		Set<UserRole> userRoles = new HashSet<>();
-		userRoles.add(new UserRole(user,new Role(RolesEnum.BASIC)));
+		userRoles.add(new UserRole(user,new Role(RolesEnum.ADMIN)));
 		LOG.debug("Creating user with username {}", user.getUsername());
 		userService.createUser(user, PlansEnum.PRO,userRoles);
 		LOG.info("user {} created",user.getUsername());
--- src/main/java/com/example/config/ApplicationConfig.java
+++ src/main/java/com/example/config/ApplicationConfig.java
@@ -2,6 +2,7 @@ package com.example.config;
 
 import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -9,5 +10,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableJpaRepositories(basePackages = "com.example.backend.persistance.repositories")
 @EntityScan(basePackages = "com.example.backend.persistance.domain.backend")
 @EnableTransactionManagement
+@PropertySource("file:///${user.home}/Desktop/Bootstrap app/webApp/application-common.properties")
 public class ApplicationConfig {
 }
